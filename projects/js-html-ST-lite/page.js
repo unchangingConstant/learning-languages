f17f68836a408ee9fa5514bf14a20a5fe634efcf
unchangingConstant <ebegleyc@gmail.com>
2025-12-09 15:24:28 -0500

    Create simple table view in JS

--- projects/js-html-ST-lite/page.js
+++ projects/js-html-ST-lite/page.js
@@ -1,5 +1,77 @@
 'use strict';
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Goooood boy.";
+class TableContent {
+
+  #changeHandler;
+  #columnNames;
+  #rows;
+
+  constructor({
+    changeHandler = ()=>{}, 
+    initRows = [], 
+    columnNames = []} = {}) {
+    this.#changeHandler = changeHandler;
+    this.#columnNames = columnNames;
+    this.#rows = initRows;   
+  }
+
+  getRows() {
+    return this.#rows;
+  }
+
+  addRow(newRow) {
+    this.#rows = [...this.#rows, TableContent.#formatRow(newRow, this.#columnNames)];
+    this.#changeHandler(this.#rows);
+  }
+
+  delRow() {
+    this.#rows.pop();
+    this.#changeHandler(this.#rows);
+  }
+
+  static #formatRow(row, columnNames) {
+    const result = {};
+    for (const column of columnNames) {
+      result[column] = row[column] !== undefined ? row[column] : null;
+    }
+    return result;
+  }
+
 }
+
+// Create model
+var tableModel = new TableContent({
+  changeHandler: updateTable,
+  columnNames: ['fullName', 'subjects']
+});
+
+// Controller
+function onAdd() {
+  tableModel.addRow({
+    fullName : 'Sample',
+    subjects : 1
+  });
+}
+
+function onDelete() {
+  tableModel.delRow()
+}
+
+// View
+function updateTable(newRows) {
+  console.log(newRows);
+  const htmlTable = document.getElementById("table");
+  htmlTable.innerHTML = '';
+  for (const row of newRows) {
+    console.log(row);
+    const htmlRow = document.createElement('tr');
+    for (const key of Object.keys(row)) {
+      const htmlTableItem = document.createElement('td');
+      htmlTableItem.textContent = row[key]
+      htmlRow.appendChild(htmlTableItem);
+    }
+    htmlTable.appendChild(htmlRow);
+  }
+  console.log(htmlTable);
+}
+
